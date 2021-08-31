7412e36c97674ca6daf86256c945e3dfaffb6b47
Pinheiro da Rocha <msph@gft.com>
2021-08-31 09:14:17 -0300

    Correcao da criptografia pelo Danilo das Neves

--- SdkApiB2bLibrary/api/security/Encryptor.cs
+++ SdkApiB2bLibrary/api/security/Encryptor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,20 +17,28 @@ namespace SdkApiB2bLibrary.api.client
 		{
 			this.ChavePublica = chavePublica;
 		}
-		public string Encript(String texto)
+		public string Encript(String textoParaCriptografar)
 		{
 			//var objChave = new System.Security.Cryptography.X509Certificates.X509Certificate2();
 			//objChave.Import(Encoding.UTF8.GetBytes(ChavePublica));
 			//var rsa = (RSACryptoServiceProvider)objChave.PublicKey.Key;
 
-			RSACryptoServiceProvider rsa = new();
+			/*RSACryptoServiceProvider rsa = new();
 			RSAParameters RSAKeyInfo = rsa.ExportParameters(false);			
 			RSAKeyInfo.Modulus = Encoding.UTF8.GetBytes(ChavePublica);		
 			rsa.ImportParameters(RSAKeyInfo);
 
 			var textoEncriptado = rsa.Encrypt(Encoding.UTF8.GetBytes(texto), false);
 			string outputData = Convert.ToBase64String(textoEncriptado);
-			return outputData;
+			return outputData;*/
+
+			var bytesChavePublicaVia = Encoding.UTF8.GetBytes(this.ChavePublica);
+			X509Certificate2 cert = new X509Certificate2(bytesChavePublicaVia);
+			RSA rsa = cert.GetRSAPublicKey();
+			var plainTextBytes = Encoding.UTF8.GetBytes(textoParaCriptografar);
+			var cipherTextBytes = rsa.Encrypt(plainTextBytes, RSAEncryptionPadding.Pkcs1);
+			string cipherText = Convert.ToBase64String(cipherTextBytes);
+			return cipherText;
 		}
 	}
 }
